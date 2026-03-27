9f633c9d91b1e10ffae6e70574d361b7b9cd783e
Thomas Dubos <thomas.dubos@polytechnique.edu>
2026-03-27 17:52:10 +0100

    Fix managed broadcast when lhs is allocated.

--- src/julia/broadcast.jl
+++ src/julia/broadcast.jl
@@ -1,20 +1,46 @@
+# support for syntax : @. mgr[a] = b + c
+# which is semantically equivalent to:
+#     @. a = b + c
+# if a is an array, otherwise to:
+#     @. b + c
+
+#====================== array lhs ======================#
+
 struct ManagedArray{A,M,T,N} <: AbstractArray{T,N}
     mgr::M
     a::A
     ManagedArray(mgr::M, a::A) where {M,A}= new{A, M, eltype(a), ndims(a)}(mgr, a)
 end
+Base.getindex(mgr::LoopManager, a::AbstractArray) = ManagedArray(mgr, a)
+Base.ndims(::Type{ManagedArray{A}}) where A = ndims(A)
+Base.size(ma::ManagedArray) = size(ma.a)
+
+@inline function Broadcast.materialize!(lhs::ManagedArray, bc::Broadcast.Broadcasted)
+    bc = Broadcast.instantiate(bc)
+    T = Broadcast.combine_eltypes(bc.f, bc.args)
+    managed_copyto!(lhs.mgr, lhs.a, bc, axes(bc)...)
+    return lhs.a
+end
+
+function Base.copyto!(ma::ManagedArray, bc::Broadcast.Broadcasted)
+    managed_copyto!(ma.mgr, ma.a, bc, axes(bc)...)
+    return ma.a
+end
+
+#====================== void lhs ======================#
 
 struct ManagedOther{Other,M}
     mgr::M
     other::Other
 end
+Base.getindex(mgr::LoopManager, other) = ManagedOther(mgr, other)
 
-Base.ndims(::Type{ManagedArray{A}}) where A = ndims(A)
-Base.size(ma::ManagedArray) = size(ma.a)
-
-function Base.copyto!(ma::ManagedArray, bc::Broadcast.Broadcasted)
-    managed_copyto!(ma.mgr, ma.a, bc, axes(bc)...)
-    return ma.a
+@inline function Broadcast.materialize!(lhs::ManagedOther, bc::Broadcast.Broadcasted)
+    bc = Broadcast.instantiate(bc)
+    T = Broadcast.combine_eltypes(bc.f, bc.args)
+    a = similar(bc, T)
+    managed_copyto!(lhs.mgr, a, bc, axes(bc)...)
+    return a
 end
 
 @loops function managed_copyto!(_, a, bc, ax1)
@@ -24,6 +50,8 @@ end
         end
     end
 end
+
+#====================== managed_copyto! ======================#
 
 @loops function managed_copyto!(_, a, bc, ax1, ax2)
     let (irange, jrange) = (ax1, ax2)
@@ -53,20 +81,4 @@ end
             end
         end
     end
-end
-
-# support for syntax : @. mgr[a] = b + c
-# which is semantically equivalent to:
-#     @. a = b + c
-# if a is an array, otherwise to:
-#     @. b + c
-Base.getindex(mgr::LoopManager, a::AbstractArray) = ManagedArray(mgr, a)
-Base.getindex(mgr::LoopManager, other) = ManagedOther(mgr, other)
-
-@inline function Broadcast.materialize!(lhs::ManagedOther, bc::Broadcast.Broadcasted)
-    bc = Broadcast.instantiate(bc)
-    T = Broadcast.combine_eltypes(bc.f, bc.args)
-    a = similar(bc, T)
-    managed_copyto!(lhs.mgr, a, bc, axes(bc)...)
-    return a
 end
